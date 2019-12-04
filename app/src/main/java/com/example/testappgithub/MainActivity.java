198429188ec420c5a88a700d3ce937d0deb25a81
User <hazemmoahmed5056@gmail.com>
2019-12-04 07:56:45 +0200

    new branch

--- app/src/main/java/com/example/testappgithub/MainActivity.java
+++ app/src/main/java/com/example/testappgithub/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
 
         Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
 
     }
 
