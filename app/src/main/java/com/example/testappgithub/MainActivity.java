5bb91aa05826d491dffb003794c4562ab325e121
User <hazemmoahmed5056@gmail.com>
2019-12-04 08:13:14 +0200

    app clonee

--- app/src/main/java/com/example/testappgithub/MainActivity.java
+++ app/src/main/java/com/example/testappgithub/MainActivity.java
@@ -26,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
 
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+
 
     }
 
